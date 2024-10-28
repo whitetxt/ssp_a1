0750fb8a0995ab6c6d0ef14494ac31acde6eec94
whitetxt <whitetxt@whitetxt.dev>
2024-10-28 14:56:00 +0000

    Added information to the animals page

--- app/Views/Animals/index.php
+++ app/Views/Animals/index.php
@@ -1,17 +1,25 @@
 <?= $this->extend("layouts/default") ?>
 <?= $this->section("content") ?>
-<div class="grid grid-cols-4 gap-8">
-    <?php foreach($animals as $animal): ?>
-    <div class="card bg-base-100 w-full shadow-xl">
-        <div class="card-body">
-            <h2 class="card-title"><?= esc($animal["name"]) ?></h2>
-            <p><?= esc($animal["description"]) ?></p>
-            <p>Species: <?= esc($animal["species"]) ?></p>
-            <p>Age: <?= esc($animal["age"]) ?></p>
-            <p>Arrival Date: <?= esc($animal["arrival_date"]) ?></p>
-            <p>Rescued: <?= $animal["is_rescued"] ? "Yes" : "No" ?></p>
-        </div>
+<div class="h-full flex flex-col gap-4">
+    <div class="prose max-w-[100%]">
+        <h1>Animals at the British Wildlife Centre</h1>
+        <p>These are some of the forty or so species on display at the Centre. You should see most of these when you visit, unless the animals are having a check-up or work being carried out on their enclosures. Just click on the links below to learn more about these species.</p>
+        <p>ref: https://britishwildlifecentre.co.uk/planyourvisit/animals/</p>
+        <p>Or use content from this page https://britishwildlifecentre.co.uk/planyourvisit/your-visit/</p>
     </div>
-    <?php endforeach; ?>
+    <div class="grid grid-cols-4 gap-8 overflow-y-auto h-full p-2 shadow-md rounded-md border border-primary">
+        <?php foreach($animals as $animal): ?>
+        <div class="card bg-base-100 w-full shadow-xl border border-secondary">
+            <div class="card-body">
+                <h2 class="card-title"><?= $animal["name"] ?></h2>
+                <p><?= $animal["description"] ?></p>
+                <p>Species: <?= $animal["species"] ?></p>
+                <p>Age: <?= $animal["age"] ?></p>
+                <p>Arrival Date: <?= $animal["arrival_date"] ?></p>
+                <p>Rescued: <?= $animal["is_rescued"] ? "Yes" : "No" ?></p>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 <?= $this->endSection() ?>
