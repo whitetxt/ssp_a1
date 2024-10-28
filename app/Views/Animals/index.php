62b91872b8debe48b84a0cc6c339c180f5aecac0
whitetxt <whitetxt@whitetxt.dev>
2024-10-28 15:17:18 +0000

    Added animal images

--- app/Views/Animals/index.php
+++ app/Views/Animals/index.php
@@ -4,12 +4,16 @@
     <div class="prose max-w-[100%]">
         <h1>Animals at the British Wildlife Centre</h1>
         <p>These are some of the forty or so species on display at the Centre. You should see most of these when you visit, unless the animals are having a check-up or work being carried out on their enclosures. Just click on the links below to learn more about these species.</p>
-        <p>ref: https://britishwildlifecentre.co.uk/planyourvisit/animals/</p>
-        <p>Or use content from this page https://britishwildlifecentre.co.uk/planyourvisit/your-visit/</p>
     </div>
     <div class="grid grid-cols-4 gap-8 overflow-y-auto h-full p-2 shadow-md rounded-md border border-primary">
         <?php foreach($animals as $animal): ?>
-        <div class="card bg-base-100 w-full shadow-xl border border-secondary">
+        <div class="card bg-base-100 image-full w-full shadow-xl border border-secondary">
+            <figure>
+                <img
+                src="/static/img/<?=$animal["image"]?>"
+                alt="<?= $animal["name"] ?>"
+                class="w-full blur"/>
+            </figure>
             <div class="card-body">
                 <h2 class="card-title"><?= $animal["name"] ?></h2>
                 <p><?= $animal["description"] ?></p>
